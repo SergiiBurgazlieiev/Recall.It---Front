18a59d35446a78f3565983dc4cae2b37caa5b1cb
Ulukbek Nusubaliev <ulukbeknusubaliev@Ulukbeks-MacBook-Air.local>
2020-03-05 11:35:15 +0600

    styling logo icon

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,10 +1,8 @@
 import React from "react";
-import icon from "../assets/images/extension_icon.png";
 
 export default ({ showResults }) => (
   <div>
     <div className="logo">
-      <img src={icon} alt="icon" onClick={() => showResults()} />
-    </div>
+      <div className="icon" onClick={() => showResults()}></div>
   </div>
 );
