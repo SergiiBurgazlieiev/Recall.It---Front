1e206609ba5437783f8733a877a60e336f2c72ba
Ulukbek Nusubaliev <ulukbeknusubaliev@Ulukbeks-MacBook-Air.local>
2020-03-08 02:38:14 +0600

    no change

--- src/features/products/components/productFilter.js
+++ src/features/products/components/productFilter.js
@@ -1,9 +1,6 @@
 import React from "react";
 
 export default ({ handleClick, prdLinks }) => {
-  const elements = ["Product", "Category", "Manufacturer"];
-  console.log(prdLinks[0]);
-  console.log(elements);
   return (
     <div className="listContainer">
       <ul className="listOfProductBtn">
