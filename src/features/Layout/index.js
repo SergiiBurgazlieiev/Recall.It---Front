5ce36f75704461151f537aec50d16b8964eba32f
annanouch <annanouchamine@gmail.com>
2020-03-10 23:06:28 +0100

    change scraper source

--- src/features/Layout/index.js
+++ src/features/Layout/index.js
@@ -5,10 +5,49 @@ import Products from "../products";
 import Result from "../../components/result";
 import { parseQueryString } from "../../utils";
 import { getProduct, scrapProduct } from "../../apis/product";
-import iconRisky from "../../assets/images/risky.png"
-import iconMed from "../../assets/images/medium.png"
-import iconNeut from "../../assets/images/neutral.png"
-import iconNon from "../../assets/images/none.png"
+import iconRisky from "../../assets/images/risky.png";
+import iconMed from "../../assets/images/medium.png";
+import iconNeut from "../../assets/images/neutral.png";
+import iconNon from "../../assets/images/none.png";
+
+let data = {
+  count_by: 1,
+  count_category: 24,
+  count_title: 0,
+  results_by: [
+    {
+      description:
+        "This recall involves all Joovy’s Zoom gray metal car seat stroller adapters. The adapters are gray with black plastic clips designed to attach infant car seats to stroller frames. The adapter frame’s dimensions are approximately 17” x 13” x 10”. Recalled car seat adapter models include 00945 for Graco, 00946 for Chicco and 00947 for Peg Perego frames. “Joovy” and the model numbers can be found on the label at the center of the end bar of the adapter.",
+      image:
+        "https://www.cpsc.gov/s3fs-public/Recall.2014.14072.Joovy%25202LARGE.jpg",
+      recalldate: "2013-12-30T00:00:00",
+      title: "Joovy Recalls Zoom Car Seat Stroller Adapter due to Fall Hazard",
+      url:
+        "https://www.cpsc.gov/Recalls/2014/Joovy-Recalls-Zoom-Car-Seat-Stroller-Adapter"
+    }
+  ],
+  results_category: [
+    {
+      description:
+        "This recall involves Graco Table2Table™ 6-in-1 highchairs with model number 1969721. The 6-in-1 highchairs convert to six different modes, including a traditional highchair, a booster seat and toddler chair and table. The highchair’s cushion is white with gold and gray polka dots. The model number is printed on a label on the underside of the toddler seat and on a label on the back of the booster seat. Graco and Table2Table highchair are also printed on the label on the underside of the toddler seat.",
+      image: "https://www.cpsc.gov/s3fs-public/Picture1_20.png",
+      recalldate: "2018-03-01T00:00:00",
+      title:
+        "Graco Recalls Highchairs Due to Fall Hazard; Sold Exclusively at Walmart",
+      url:
+        "https://www.cpsc.gov/Recalls/2018/Graco-Recalls-Highchairs-Due-to-Fall-Hazard-Sold-Exclusively-at-Walmart"
+    },
+    {
+      description:
+        "This recall includes Safety 1st Wood Décor highchairs in three models: HC144BZF (Casablanca), HC229CZF (Gentle Lace) and HC229CYG (Black Lace). The model numbers are printed under the highchair seat. These A-frame black wood highchairs have a removable fabric, black and white print seat pad with a blue or pink border on the top and bottom of the seat pad. The highchairs have a white plastic, detachable tray with a cone-shaped center divider that fits between a child’s legs. “Safety 1st” is printed on the front center of the tray.",
+      image: "https://www.cpsc.gov/s3fs-public/Recall.2016.16006.Highchair.jpg",
+      recalldate: "2015-10-08T00:00:00",
+      title: "Safety 1st Recalls Décor Wood Highchairs Due to Fall Hazard",
+      url:
+        "https://www.cpsc.gov/Recalls/2016/Safety-1st-Recalls-Decor-Wood-Highchair"
+    }
+  ]
+};
 
 export default () => {
   const [displayProductsResult, setDisplayProductsResult] = useState(false);
@@ -95,14 +134,18 @@ export default () => {
       });
       setState({ productsByName, productsByType, productsByManufacturer });
 
-      if(productsByManufacturer.length > 5 && productsByType.length > 5){
+      if (productsByManufacturer.length > 5 && productsByType.length > 5) {
         setIcon(iconMed);
-      }else if(productsByName.length > 0){
+      } else if (productsByName.length > 0) {
         setIcon(iconRisky);
-      }else if(productsByName.length == 0 && productsByType.length ==0 && productsByManufacturer.length == 0){
-        setIcon(iconNon)
-      }else{
-        setIcon(iconNeut)
+      } else if (
+        productsByName.length === 0 &&
+        productsByType.length === 0 &&
+        productsByManufacturer.length === 0
+      ) {
+        setIcon(iconNon);
+      } else {
+        setIcon(iconNeut);
       }
     } catch (err) {}
   };
@@ -114,7 +157,6 @@ export default () => {
 
   useEffect(() => {
     let params = parseQueryString();
-
     (async () => {
       try {
         let data = await scrapProduct({ url: params.url });
@@ -127,14 +169,14 @@ export default () => {
             categorie: data.categories[categories.length],
             isDataRequest: false
           });
-        else
+        /*else
           setProduct({
             image: params.image,
             name: params.productTitle,
             by: params.by,
             categorie: params.categorie,
             isDataRequest: false
-          });
+          });*/
       } catch (e) {
         setProduct({
           image: params.image,
