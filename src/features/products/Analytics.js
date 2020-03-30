285123da9badd8125bb766e1bf087481778168d7
Sergii Burgazlieiev <burgazlieiev@gmail.com>
2020-03-30 11:25:07 -0500

    fix

--- src/features/products/Analytics.js
+++ src/features/products/Analytics.js
@@ -6,7 +6,7 @@ import styles from './Analytics.module.css';
 const App = () => {
     return (
         <div className={styles.Analytics}>
-            <Neiss/> 
+            {/* <Neiss/>  */}
         </div>
     );
 }
