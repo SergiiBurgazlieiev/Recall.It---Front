d6078a7aea01783607764e37c1948cff7b6c5e20
Ulukbek Nusubaliev <ulukbeknusubaliev@Ulukbeks-MacBook-Air.local>
2020-04-03 12:48:08 +0600

    test

--- src/components/logo.js
+++ src/components/logo.js
@@ -19,12 +19,16 @@ export default ({ showResults, icon, requested }) => {
   					</div>
  				</div>
 	}else{
-		return  <div className="logo_window">
+		return
+			<>  
+				<div className="resultWindow">
+					<span className={btnClass}>We are gathering results for you ...</span>
+				</div>
     				<div className="logo">
-    					<span className="loading">We are loading</span>
       					<img src={icon} alt="icon" onClick={() => showResults()} />
   					</div>
  				</div>
+ 			</>
 	}
 			
 };
